2b5ca01572ef161ecf648a17019829737b1a7a90
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-02-12 20:25:00 -0600

    Add files via upload
    LOGIN TERMUX
    Esta es una herramienta creada por Scorpio28, cuya función es un login para poder ingresar a la terminal dando un elegante banner de la comunidad Informatic in Termux.
    
    INSTALL IN TERMUX
    
    $ git clone https://github.com/Scorpio28-Oficial/shell-login/pulls.git
    
    $ cd shell-login
    
    $ ls
    
    $ chmod +x setup.sh login.sh
    
    $ bash setup.sh
    
    $ bash login.sh
    
    Canal de Youtube
    
    https://youtube.com/channel/UCZxKLn6KG0Dm5kmRaOGphsg

---
+++ setup.sh
@@ -0,0 +1,21 @@
+echo "-----------------------------------------------------"
+echo "          TERMUX LOGIN BY Scorpio28.        "
+echo "It will take some time to install please be patience"
+echo "-----------------------------------------------------"
+
+
+cp login.sh $PREFIX/etc
+apt update
+pkg install cmatrix
+apt-get install -y ruby
+apt-get install -y espeak
+apt install python
+pip install lolcat
+ 
+
+cd
+
+
+bash login.sh
+
+cd
